a4bc40ace942320d844ba8eb2291fa0bc2430313
Miles <miles.j.hoo@gmail.com>
2021-12-11 11:58:45 -0600

    add merge sort

--- src/main/java/part1/chapter2/InsertionSort.java
+++ src/main/java/part1/chapter2/InsertionSort.java
@@ -1,23 +1,23 @@
-package part1.chapter1;
+package part1.chapter2;
 
 public class InsertionSort {
 
-    public static int[] insertionSort(int[] arr) {
+    public static void insertionSort(int[] arr) {
         int n = arr.length;
         for (int j = 1; j < n; j++) {
             int key = arr[j], i = j - 1;
+            //Insert A[j] into the sorted sequence A[0..j - 1].
             while (i >= 0 && arr[i] > key) {
                 arr[i + 1] = arr[i];
                 i--;
             }
             arr[i + 1] = key;
         }
-        return arr;
     }
 
     public static void main(String[] args) {
         int[] arr = {4, 3, 9, 5, 1, 8, 2, 7, 6, 0};
-        arr = insertionSort(arr);
+        insertionSort(arr);
         StringBuilder sb = new StringBuilder();
         int n = arr.length;
         for (int i = 0; i < n; i++) {
---
+++ src/main/java/part1/chapter2/MergeSort.java
@@ -0,0 +1,52 @@
+package part1.chapter2;
+
+public class MergeSort {
+
+    public static void mergeSort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1); // divide
+        sort(arr, l, mid); // solve
+        sort(arr, mid + 1, r);
+        merge(arr, l, mid, r); // merge
+    }
+
+    private static void merge(int[] arr, int l, int mid, int r) {
+        int len = r - l + 1;
+        int[] auxArr = new int[len];
+        int index = 0, i = l, j = mid + 1;
+        while (i <= mid || j <= r) {
+            if (i > mid) {
+                auxArr[index++] = arr[j++];
+            } else if (j > r) {
+                auxArr[index++] = arr[i++];
+            } else if (arr[i] <= arr[j]) {
+                auxArr[index++] = arr[i++];
+            } else {
+                auxArr[index++] = arr[j++];
+            }
+        }
+        for (i = 0; i < len; i++) {
+            arr[i + l] = auxArr[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 9, 5, 1, 8, 2, 7, 6, 0};
+        mergeSort(arr);
+        StringBuilder sb = new StringBuilder();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]);
+            if (i != n - 1) {
+                sb.append(", ");
+            }
+        }
+        System.out.println(sb);
+    }
+}
